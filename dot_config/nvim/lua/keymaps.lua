15a98ef0c835b003e6f64144d474396933fd44f6
Denis Buzmakov <me@bacecek.dev>
2025-06-16 09:25:19 +0200

    Update .config/nvim/lua/keymaps.lua Update .config/karabiner/karabiner.json

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -7,3 +7,7 @@ map('n', '<leader>f', vim.lsp.buf.format, { desc = "Format document" })
 
 map("n", "<Leader>z", ":terminal zsh -i<CR>")
 map("t", "<Esc>", "<C-\\><C-n>")
+
+map('v', '<leader>y', '"+y', { noremap = true, silent = true })
+map('n', '<leader>p', '"+p', { noremap = true, silent = true })
+
